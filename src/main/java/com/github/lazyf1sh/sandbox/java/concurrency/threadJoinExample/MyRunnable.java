ba7d6b6fe71f001d91db6e1ebb7714e5d8a15278
lazyf1sh <1>
2018-04-01 17:12:34 +0300

    Thread join(ms) example

---
+++ src/main/java/com/github/lazyf1sh/sandbox/java/concurrency/threadJoinExample/MyRunnable.java
@@ -0,0 +1,47 @@
+package com.github.lazyf1sh.sandbox.java.concurrency.threadJoinExample;
+
+public class MyRunnable implements Runnable
+{
+	private String name;
+	private Thread thread;
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public Thread getThread()
+	{
+		return thread;
+	}
+
+	public void setThread(Thread thread)
+	{
+		this.thread = thread;
+	}
+
+	public MyRunnable(String threadName)
+	{
+		this.name = threadName;
+		thread = new Thread(this, threadName);
+		thread.start();
+	}
+
+	public void run()
+	{
+		try
+		{
+			Thread.sleep(5000);
+			System.out.println(this.name);
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
