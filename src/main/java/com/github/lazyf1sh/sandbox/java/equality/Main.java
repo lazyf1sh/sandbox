ff1b89a1f3cf755ea20a9287736b6e626a22fa6b
lazyf1sh <1>
2018-06-10 09:24:54 +0300

    upd

--- src/main/java/com/github/lazyf1sh/sandbox/java/equality/Main.java
+++ src/main/java/com/github/lazyf1sh/sandbox/java/equality/Main.java
@@ -1,13 +1,18 @@
 package com.github.lazyf1sh.sandbox.java.equality;
 
+import java.util.Date;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
 		Foo f = new Foo();
 		boolean b = f.equals(null);
+
+		Date d = new Date();
+		d.equals(null);
+
 		int i = 0;
-		//
 	}
 
 }
