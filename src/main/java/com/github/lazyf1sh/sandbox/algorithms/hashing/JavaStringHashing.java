4e70e9cde41c3225a9247dd230ef7c3ddc695b55
lazyf1sh <f1sh@pm.me>
2018-06-25 22:08:19 +0300

    java hashing sandbox

---
+++ src/main/java/com/github/lazyf1sh/sandbox/algorithms/hashing/JavaStringHashing.java
@@ -0,0 +1,10 @@
+package com.github.lazyf1sh.sandbox.algorithms.hashing;
+
+public class JavaStringHashing
+{
+	public static void main(String[] args)
+	{
+		String s = "12";
+			s.hashCode();
+	}
+}
