05f4b7e97104cd8779c8cf751953c0f6d4436106
lazyf1sh <f1sh@pm.me>
2019-07-10 17:50:12 +0300

    upd

---
+++ src/test/java/com/github/lazyf1sh/sandbox/java/streams/StreamsNullSafetyTest.java
@@ -0,0 +1,19 @@
+package com.github.lazyf1sh.sandbox.java.streams;
+
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * Purpose of this example: to show that it is not safe to get stream from null object
+ * @author Ivan Kopylov
+ */
+public class StreamsNullSafetyTest
+{
+    @Test(expected = NullPointerException.class)
+    public void run()
+    {
+        List<String> myList = null;
+        myList.stream().forEach(System.out::println);
+    }
+}
