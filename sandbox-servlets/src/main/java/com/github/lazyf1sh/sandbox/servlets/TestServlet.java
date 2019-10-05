6636cb3666744e07a4768f1b5495e65e194d34a0
lazyf1sh <f1sh@pm.me>
2019-10-05 10:47:08 +0300

    migrated java servlets examples

---
+++ sandbox-servlets/src/main/java/com/github/lazyf1sh/sandbox/servlets/TestServlet.java
@@ -0,0 +1,20 @@
+package com.github.lazyf1sh.sandbox.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+
+    private static final long serialVersionUID = -873386876696046661L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        System.out.println(req.getMethod());
+    }
+}
