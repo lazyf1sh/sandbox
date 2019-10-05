5574b4a88d42fffe1c44dfab594d98f74a7bf4bc
lazyf1sh <f1sh@pm.me>
2019-10-05 11:34:14 +0300

    upd

---
+++ sandbox-javafx/src/main/java/com/github/lazyf1sh/sandbox/java/jcl/javafx/Controller.java
@@ -0,0 +1,11 @@
+package com.github.lazyf1sh.sandbox.java.jcl.javafx;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void sayHelloWorld(ActionEvent actionEvent) {
+
+
+
+    }
+}
