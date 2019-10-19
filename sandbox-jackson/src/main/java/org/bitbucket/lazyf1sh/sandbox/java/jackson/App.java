516497547129d28c1d9086c5840d1d8fa3bfda7a
lazyf1sh <f1sh@pm.me>
2019-10-19 22:24:21 +0100

    added jackson serializer example

---
+++ sandbox-jackson/src/main/java/org/bitbucket/lazyf1sh/sandbox/java/jackson/App.java
@@ -0,0 +1,34 @@
+package org.bitbucket.lazyf1sh.sandbox.java.jackson;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws JsonProcessingException
+    {
+        List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+
+        Map<String, String> m = new HashMap<String, String>();
+        m.put("k1", "v1");
+        m.put("k2", "v2");
+        list.add(m);
+
+        m = new HashMap<String, String>();
+        m.put("k3", "v3");
+        m.put("k4", "v4");
+        list.add(m);
+
+        String s = new ObjectMapper().writeValueAsString(list);
+        System.out.println(s);
+    }
+}
