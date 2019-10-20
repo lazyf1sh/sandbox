e9cb5bc16ac29c88b53988cfcbeca8d96be649f1
lazyf1sh <f1sh@pm.me>
2019-10-20 16:12:54 +0100

    added liquibase example run with maven

---
+++ sandbox-java/src/test/java/com/github/lazyf1sh/sandbox/algorithms/RevertedIfConditionTest.java
@@ -0,0 +1,32 @@
+package com.github.lazyf1sh.sandbox.algorithms;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Ivan Kopylov
+ */
+
+public class RevertedIfConditionTest
+{
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void run()
+    {
+        Assert.assertTrue(RevertedIfCondition.run(true, false));
+        Assert.assertFalse(RevertedIfCondition.run(true, true));
+        Assert.assertFalse(RevertedIfCondition.run(false, true));
+        Assert.assertFalse(RevertedIfCondition.run(false, false));
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void run2()
+    {
+        Assert.assertTrue(RevertedIfCondition.run2(true, false));
+        Assert.assertFalse(RevertedIfCondition.run2(true, true));
+        Assert.assertFalse(RevertedIfCondition.run2(false, true));
+        Assert.assertFalse(RevertedIfCondition.run2(false, false));
+    }
+}
