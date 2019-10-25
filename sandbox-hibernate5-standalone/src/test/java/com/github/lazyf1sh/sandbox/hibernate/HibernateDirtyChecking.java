557ab75e8290ff14ed6393611e337b30dd12ba2c
lazyf1sh <f1sh@pm.me>
2019-10-25 21:01:19 +0300

    added hibernate dirty checking example

---
+++ sandbox-hibernate5-standalone/src/test/java/com/github/lazyf1sh/sandbox/hibernate/HibernateDirtyChecking.java
@@ -0,0 +1,70 @@
+package com.github.lazyf1sh.sandbox.hibernate;
+
+import com.github.lazyf1sh.sandbox.persistence.entities.BookEntity;
+import com.github.lazyf1sh.sandbox.persistence.util.HibernateSessionFactory;
+import org.hibernate.Session;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HibernateDirtyChecking
+{
+    @Test
+    public void dirtyChecking()
+    {
+        triggerDirtyCheck();
+        verifyDirtyCheckWorks();
+    }
+
+    public void triggerDirtyCheck()
+    {
+        Session session = HibernateSessionFactory.getSession();
+        session.getTransaction().begin();
+
+        BookEntity book = session.find(BookEntity.class, 0);
+        if (book != null)
+        {
+            book.setName("Fred Brooks - The Mythical Man-Month");
+            //session.update(book);//Even though you comment this line still
+            // hibernate triggers update query if book name is different
+            //in the database this feature is called dirty checking in hibernate
+        }
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public static void verifyDirtyCheckWorks()
+    {
+        Session session = HibernateSessionFactory.getSession();
+        session.getTransaction().begin();
+
+        BookEntity book = session.find(BookEntity.class, 0);
+        if (book != null)
+        {
+            Assert.assertEquals("Fred Brooks - The Mythical Man-Month", book.getName());
+        }
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+
+    @BeforeClass
+    public static void populate()
+    {
+        Session session = HibernateSessionFactory.getSession();
+        session.getTransaction().begin();
+
+        BookEntity book = new BookEntity();
+        book.setName("Maxim Dorofeev - Inbox Zero");
+        book.setId(0);
+
+        session.persist(book);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+
+}
