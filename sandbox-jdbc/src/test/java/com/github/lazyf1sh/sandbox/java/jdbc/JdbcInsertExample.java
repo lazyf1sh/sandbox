2931f06b03fab97ee267503bee9a89f0b50b55e0
lazyf1sh <f1sh@pm.me>
2019-11-09 13:17:59 +0100

    added prepared statement 1+1 injection example

--- sandbox-jdbc/src/test/java/com/github/lazyf1sh/sandbox/java/jdbc/JdbcInsertExample.java
+++ sandbox-jdbc/src/test/java/com/github/lazyf1sh/sandbox/java/jdbc/JdbcInsertExample.java
@@ -5,38 +5,81 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Random;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class JdbcInsertExample
 {
-    private static final String SQL_INSERT = "INSERT INTO PARENTTABLE (PARENTTABLE_KEY, PARENTTABLE_NAME) VALUES (?,?)";
+    private static final String CREATE_PARENTTABLE = "CREATE TABLE PARENTTABLE (PARENTTABLE_KEY INTEGER NOT NULL, PARENTTABLE_NAME VARCHAR(255), PRIMARY KEY (PARENTTABLE_KEY))";
+    private static final String SQL_INSERT         = "INSERT INTO PARENTTABLE (PARENTTABLE_KEY, PARENTTABLE_NAME) VALUES (?,?)";
 
-    @Test
-    public void run()
+    @BeforeClass
+    public static void create_table() throws SQLException
     {
-        try (Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://127.0.0.1:3306/sandbox_hibernate?serverTimezone=UTC", "hibernate", "hibernate");
-                PreparedStatement preparedStatement = conn.prepareStatement(SQL_INSERT))
+        Connection conn = null;
+        Statement preparedStatement = null;
+        try
         {
+            conn = DriverManager.getConnection("jdbc:h2:mem:myDb;DB_CLOSE_DELAY=-1");
+            preparedStatement = conn.createStatement();
+
+            int i = preparedStatement.executeUpdate(CREATE_PARENTTABLE);
+            Assert.assertEquals(0, i);
+        }
+        finally
+        {
+            if (preparedStatement != null)
+            {
+                preparedStatement.close();
+            }
+            if (conn != null)
+            {
+                conn.close();
+            }
+        }
+    }
+
+    /**
+     * PreparedStatement are:<br/>
+     * 1. Precompiled.<br/>
+     * 2. Injection-safe.<br/>
+     */
+    @Test
+    public void run() throws SQLException
+    {
+        Connection conn = null;
+        PreparedStatement preparedStatement = null;
+
+        try
+        {
+            conn = DriverManager.getConnection("jdbc:h2:mem:myDb;DB_CLOSE_DELAY=-1");
+            preparedStatement = conn.prepareStatement(SQL_INSERT);
             Random random = new Random();
             preparedStatement.setInt(1, random.nextInt());
-            preparedStatement.setBigDecimal(2, new BigDecimal(799.88));
+            preparedStatement.setString(2, "string value");
 
             int row = preparedStatement.executeUpdate();
 
             // rows affected
             Assert.assertEquals(1, row);
+
+            preparedStatement.close();
+            conn.close();
         }
-        catch (SQLException e)
+        finally
         {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
+            if (preparedStatement != null)
+            {
+                preparedStatement.close();
+            }
+            if (conn != null)
+            {
+                conn.close();
+            }
         }
     }
 }
---
+++ sandbox-jdbc/src/test/java/com/github/lazyf1sh/sandbox/java/jdbc/JdbcPreparedStatementInjection.java
@@ -0,0 +1,142 @@
+package com.github.lazyf1sh.sandbox.java.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class JdbcPreparedStatementInjection
+{
+    private static final String CREATE_PARENTTABLE  = "CREATE TABLE GEMS (GEM_KEY INTEGER NOT NULL, NAME VARCHAR(255), PRIMARY KEY (GEM_KEY))";
+    private static final String SQL_INSERT_TEMPLATE = "INSERT INTO GEMS (GEM_KEY, NAME) VALUES (?,?)";
+
+    @BeforeClass
+    public static void create_table_populate() throws SQLException
+    {
+        Connection conn = null;
+        Statement statement = null;
+        PreparedStatement preparedStatement = null;
+        try
+        {
+            conn = DriverManager.getConnection("jdbc:p6spy:h2:mem:myDb;DB_CLOSE_DELAY=-1");
+            statement = conn.createStatement();
+
+            int lines = statement.executeUpdate(CREATE_PARENTTABLE);
+            Assert.assertEquals(0, lines);
+
+            preparedStatement = conn.prepareStatement(SQL_INSERT_TEMPLATE);
+            preparedStatement.setInt(1, 0);
+            preparedStatement.setString(2, "Diamond");
+            preparedStatement.executeUpdate();
+
+            preparedStatement = conn.prepareStatement(SQL_INSERT_TEMPLATE);
+            preparedStatement.setInt(1, 1);
+            preparedStatement.setString(2, "Opal");
+            preparedStatement.executeUpdate();
+
+        }
+        finally
+        {
+            if (statement != null)
+            {
+                statement.close();
+            }
+            if (conn != null)
+            {
+                conn.close();
+            }
+        }
+    }
+
+    @Test
+    public void injection_sucess() throws SQLException
+    {
+        Connection conn = null;
+        Statement statement = null;
+
+        try
+        {
+            conn = DriverManager.getConnection("jdbc:p6spy:h2:mem:myDb;DB_CLOSE_DELAY=-1");
+            statement = conn.createStatement();
+            Random random = new Random();
+
+            //            String name_to_find = "Diamond";
+            String name_to_find = "Diamond' OR 1+1 OR '";
+
+            int count = 0;
+            ResultSet rs = statement.executeQuery("SELECT * FROM GEMS WHERE NAME like '" + name_to_find + "';");
+            while (rs.next())
+            {
+                count++;
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                System.out.println(String.format("id: %s, name: %s", id, name));
+            }
+            Assert.assertEquals(2, count);
+
+            statement.close();
+            conn.close();
+        }
+        finally
+        {
+            if (statement != null)
+            {
+                statement.close();
+            }
+            if (conn != null)
+            {
+                conn.close();
+            }
+        }
+    }
+
+    @Test
+    public void injection_prevented() throws SQLException
+    {
+        Connection conn = null;
+        PreparedStatement preparedStatement = null;
+
+        try
+        {
+            conn = DriverManager.getConnection("jdbc:p6spy:h2:mem:myDb;DB_CLOSE_DELAY=-1");
+            preparedStatement = conn.prepareStatement("SELECT * FROM GEMS WHERE NAME like ?");
+
+            //            String name_to_find = "Diamond";
+            String name_to_find = "Diamond' OR 1+1 OR '";
+            //            String name_to_find = "Diamond OR 1+1";
+            preparedStatement.setString(1, name_to_find);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            int count = 0;
+            while (rs.next())
+            {
+                count++;
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                System.out.println(String.format("id: %s, name: %s", id, name));
+            }
+            Assert.assertEquals(0, count);
+
+            preparedStatement.close();
+            conn.close();
+        }
+        finally
+        {
+            if (preparedStatement != null)
+            {
+                preparedStatement.close();
+            }
+            if (conn != null)
+            {
+                conn.close();
+            }
+        }
+    }
+}
