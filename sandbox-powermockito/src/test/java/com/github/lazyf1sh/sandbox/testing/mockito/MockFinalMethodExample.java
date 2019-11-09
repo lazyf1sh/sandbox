ca086e409244417214acc5d8574c34902e80b5d9
lazyf1sh <f1sh@pm.me>
2019-11-09 18:18:09 +0100

    upd

---
+++ sandbox-powermockito/src/test/java/com/github/lazyf1sh/sandbox/testing/mockito/MockFinalMethodExample.java
@@ -0,0 +1,24 @@
+package com.github.lazyf1sh.sandbox.testing.mockito;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.github.lazyf1sh.sandbox.testing.mockito.domain.Bottle;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Bottle.class)
+public class MockFinalMethodExample
+{
+    @Test
+    public void mockfinal()
+    {
+        Bottle bottle = PowerMockito.mock(Bottle.class);
+        PowerMockito.when(bottle.getBottle()).thenReturn("cup");
+
+        Assert.assertEquals("cup", bottle.getBottle());
+    }
+}
