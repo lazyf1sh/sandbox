5505bfeafade8ed6c72139da5bede6d4d8b59138
Ivan K <f1sh@pm.me>
2020-01-11 16:18:19 +0300

    upd

--- sandbox-java/src/test/java/com/github/lazyf1sh/sandbox/java/mechanics/signature/JavaMethodSignature.java
+++ sandbox-java/src/test/java/com/github/lazyf1sh/sandbox/java/mechanics/signature/JavaMethodSignature.java
@@ -5,7 +5,9 @@ package com.github.lazyf1sh.sandbox.java.mechanics.signature;
  * In computer science, a type signature or type annotation defines the inputs and outputs for a function, subroutine or method.<br>
  * @author Ivan Kopylov
  * @see <a href="https://i.imgur.com/hQByRA4.png">https://i.imgur.com/hQByRA4.png</a>
+ * @see <a href="https://i.stack.imgur.com/NLlbQ.png">https://i.stack.imgur.com/NLlbQ.png</a>
  * @see <a href="https://i.imgur.com/eJlX12H.png">https://i.imgur.com/eJlX12H.png</a>
+ * @see <a href="https://i.stack.imgur.com/H1nAC.png">https://i.stack.imgur.com/H1nAC.png</a>
  * @see <a href="https://en.wikipedia.org/wiki/Type_signature">https://en.wikipedia.org/wiki/Type_signature</a>
  */
 public class JavaMethodSignature
