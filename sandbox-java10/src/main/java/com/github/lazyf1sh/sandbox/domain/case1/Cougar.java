25722e959a10504bb0ca3dad21aeddb61e2a1180
Ivan K <f1sh@pm.me>
2020-05-17 17:43:40 +0300

    upd

---
+++ sandbox-java10/src/main/java/com/github/lazyf1sh/sandbox/domain/case1/Cougar.java
@@ -0,0 +1,17 @@
+package com.github.lazyf1sh.sandbox.domain.case1;
+
+//public class Cougar implements HasTail
+//{
+//    public static void main(String[] args)
+//    {
+//        var puma = new Puma() {};
+////        System.out.println(puma.get);
+//
+//    }
+//
+//
+//    public int getTailLength(int length)
+//    {
+//        return 0;
+//    }
+//}
---
+++ sandbox-java10/src/main/java/com/github/lazyf1sh/sandbox/domain/case1/Puma.java
@@ -0,0 +1,10 @@
+package com.github.lazyf1sh.sandbox.domain.case1;
+
+public abstract class Puma
+{
+    protected int getTailLength()
+    {
+        return 4;
+    }
+
+}
