cdce334acee46ebe385576e8165ebc7dbb8a8777
lazyf1sh <f1sh@pm.me>
2020-09-03 15:13:38 +0300

    upd

---
+++ sandbox-maven-owasp/owasp_jasperreports_6.14.bat
@@ -0,0 +1,2 @@
+mvn org.owasp:dependency-check-maven:check --f pom-jasperreports-6.14.xml
+@PAUSE
---
+++ sandbox-maven-owasp/owasp_pdfbox-2.0.19.bat
@@ -0,0 +1,2 @@
+mvn org.owasp:dependency-check-maven:check --f pom-pdfbox-2.0.19.xml > result
+@PAUSE
