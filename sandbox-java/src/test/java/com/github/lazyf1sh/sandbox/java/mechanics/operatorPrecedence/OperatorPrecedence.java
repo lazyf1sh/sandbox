50b50630036b81427e34755c3fb2cfedd1b829d8
lazyf1sh <f1sh@pm.me>
2020-09-11 14:23:11 +0300

    1

---
+++ sandbox-java/src/test/java/com/github/lazyf1sh/sandbox/java/mechanics/operatorPrecedence/OperatorPrecedence.java
@@ -0,0 +1,51 @@
+package com.github.lazyf1sh.sandbox.java.mechanics.operatorPrecedence;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Operators evaluation order.
+ */
+public class OperatorPrecedence
+{
+    @Test
+    public void calculateYourselfFirst1()
+    {
+        int x = 5;
+        int y = 10;
+        int result = ++x * y--;
+//        Assert.assertEquals(, result);
+    }
+
+    @Test
+    public void calculateYourselfFirst2()
+    {
+        String result1 = "1 + 2 = " + 1 + 2;
+        String result2 = "1 + 2 = " + (1 + 2);
+
+//        Assert.assertEquals("1 + 2 = ", result1);
+//        Assert.assertEquals("1 + 2 = ", result2);
+    }
+
+    @Test
+    public void calculateYourselfFirst4()
+    {
+        String result1 = 1 + 2 + "abc";
+        String result2 = "abc" + 1 + 2;
+
+        Assert.assertEquals("", result1);
+        Assert.assertEquals("", result2);
+    }
+
+
+    /**
+     * Also, add clarifying parentheses.
+     */
+    @Test
+    public void calculateYourselfFirst3()
+    {
+        int year = 2020;
+        boolean result = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+//        Assert.assertEquals(, result);
+    }
+}
