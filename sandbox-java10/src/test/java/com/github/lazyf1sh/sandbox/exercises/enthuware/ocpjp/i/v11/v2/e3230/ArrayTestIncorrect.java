9fbf878676dcfbb670e1abcf0fb9c4278ffc7070
Ivan K <f1sh@pm.me>
2020-09-14 09:58:50 +0300

    upd

---
+++ sandbox-java10/src/test/java/com/github/lazyf1sh/sandbox/exercises/enthuware/ocpjp/i/v11/v2/e3230/ArrayTestIncorrect.java
@@ -0,0 +1,14 @@
+package com.github.lazyf1sh.sandbox.exercises.enthuware.ocpjp.i.v11.v2.e3230;
+
+class ArrayTestIncorrect
+{
+//    public static void main(String[] args)
+//    {
+//        var ia[][] = {{1, 2}, null};
+//        for (int i = 0; i < 2; i++)
+//            for (int j = 0; j < 2; j++)
+//            {
+//                System.out.println(ia[i][j]);
+//            }
+//    }
+}
---
+++ sandbox-java8/src/test/java/com/github/lazyf1sh/sandbox/java/exercises/enthuware/ocpjp/i/v11/v2/e3230/ArrayNPE.java
@@ -0,0 +1,6 @@
+package com.github.lazyf1sh.sandbox.java.exercises.enthuware.ocpjp.i.v11.v2.e3230;
+
+public class ArrayNPE
+{
+    
+}
