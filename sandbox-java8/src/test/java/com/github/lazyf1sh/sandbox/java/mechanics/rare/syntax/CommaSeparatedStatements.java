90daafcf68546fce2ecbcc14bc2ed94a45255339
Ivan K <f1sh@pm.me>
2020-11-14 15:22:03 +0300

    added assignment inside expression example.

--- sandbox-java8/src/test/java/com/github/lazyf1sh/sandbox/java/mechanics/rare/syntax/CommaSeparatedStatements.java
+++ sandbox-java8/src/test/java/com/github/lazyf1sh/sandbox/java/mechanics/rare/syntax/CommaSeparatedStatements.java
@@ -1,5 +1,6 @@
 package com.github.lazyf1sh.sandbox.java.mechanics.rare.syntax;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CommaSeparatedStatements
@@ -11,5 +12,16 @@ public class CommaSeparatedStatements
         float mod1 = foo % baz, mod2 = baz % foo;  //2
         float result = mod1 > mod2 ? bar : baz; //3
 //        Assert.assertEquals(, result); //calculate yourself
+    }
+
+    @Test
+    public void assignmentInsideExpression()
+    {
+        int i1 = 1;
+        int i2 = 2;
+        int i3 = 3;
+
+        int i4 = i1 + (i2 = i3);
+        Assert.assertEquals(4, i4);
     }
 }
