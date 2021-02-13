34e00e71cee083c5ff7bc11ddaf7a68b0fd1b71b
Ivan K <f1sh@pm.me>
2021-02-13 18:56:01 +0300

    removed obsolete

---
+++ sandbox-java/src/test/java/com/github/lazyf1sh/sandbox/java/jcl/java/util/SortedSetExample.java
@@ -0,0 +1,21 @@
+package com.github.lazyf1sh.sandbox.java.jcl.java.util;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class SortedSetExample
+{
+    @Test
+    public void run()
+    {
+        new HashMap<>();
+
+        SortedSet<String> mySet = new TreeSet<>();
+        mySet.add("a");
+        mySet.add("1");
+
+    }
+}
---
+++ sandbox-java/src/test/java/com/github/lazyf1sh/sandbox/java/nullValue/NullValueExamples.java
@@ -0,0 +1,16 @@
+package com.github.lazyf1sh.sandbox.java.nullValue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NullValueExamples
+{
+    /**
+     * Demonstration that null equals null.
+     */
+    @Test
+    public void nullEqualsNull()
+    {
+        Assert.assertTrue(null == null); // true
+    }
+}
